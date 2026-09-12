7eddd036e756ecbd7f2500f754ea40980726a955
gunupatibhanuprakashreddy <bhanugunupati@gmail.com>
2026-09-12 19:31:23 +0530

    backend setup

---
+++ backend/src/test/java/com/infotact/polyglotmesh/PolyglotmeshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infotact.polyglotmesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyglotmeshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
